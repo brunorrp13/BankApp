d0145ddfac53c862249a8087c71bb47508ea8f55
brunorrp13 <brunorrp13@hotmail.com>
2021-05-30 12:08:32 -0300

    BankApp - Primeira Tela (design)

--- app/src/androidTest/java/com/example/bankapp/BankAppTest.kt
+++ app/src/androidTest/java/com/example/bankapp/BankAppTest.kt
@@ -2,11 +2,13 @@ package com.example.bankapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +16,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class BankAppTest {
+
+    val mActivityRule = ActivityTestRule(MainActivity::class.java)
+        @Rule get
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.bankapp", appContext.packageName)
     }
+
+
 }
